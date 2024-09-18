9c95e0fc61cad0a5d8e65c8c7001d9c284c033c1
Sunny Master <master.codeworks@gmail.com>
2024-09-17 23:38:49 -0400

    complete part 1 of the lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  # route for babies index
+  path('babies/', views.baby_index, name='baby-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+class Baby:
+  def __init__(self, name, diet, description, age):
+    self.name = name
+    self.diet = diet
+    self.description = description
+    self.age = age
+
+babies = [
+  Baby('April', 'vegetarian', 'always playful.', 1),
+  Baby('May', 'vegan', 'likes to fiddle with things.', 3),
+  Baby('June', 'lactose-free', 'hyperactive at all times.', 2),
+  Baby('August', 'nut-free', 'calm and curious.', 0)
+]
 # Create your views here.
 
 def home(request):
   return HttpResponse('<h1>Hello 🚼</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the DayCare</h1>')
+  return render(request, 'about.html')
+
+def baby_index(request):
+  return render(request, 'babies/index.html', { 'babies': babies })
